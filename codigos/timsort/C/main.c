f4bd95f680530d1596adfa015d568b414be3c9be
ThalesGuerreiro <thalesguerreiroc@gmail.com>
2026-05-06 00:17:51 -0300

    Acrescenta benchmark do timsort E váriaçoes de vetores

--- codigos/timsort/C/main.c
+++ codigos/timsort/C/main.c
@@ -4,39 +4,143 @@
 #include <string.h>
 #include "timsort.h"
 #include "io.h"
-#define TAMANHO_VETOR 1000000
-
-//Analise do vetor na forma aleatória - .dat cru
-//vetor ordenado - depois da ordenação
-//vetor invertido - inverte o ordenado
-//valores semlhantes - arquivo separado
+#define TAMANHO_VETOR_MAX 1000000
 
 int main(){
+    struct timespec start, end;
+    double tempo;
+    double total;
+    int tamanho = 100; //1000, 10000, 100000, 1000000
 
-    int *vetor = ler_arquivo("../../../Dados/valores.dat", TAMANHO_VETOR);
-
+    int *vetor = ler_arquivo("../../../Dados/valores.dat", TAMANHO_VETOR_MAX);
     if (vetor == NULL) {
+        printf("Erro na leitura do arquivo!\n");
         return 1;
     }
 
-    struct timespec start, end;
-
-    //-----------------------Ordenação vetor aleatório-----------------------
-    int *copia = malloc(TAMANHO_VETOR * sizeof(int));
+    int *copia = malloc(TAMANHO_VETOR_MAX * sizeof(int));
     if (copia == NULL) {
         printf("Erro de memória!\n");
         return 1;
     }
-    memcpy(copia, vetor, TAMANHO_VETOR * sizeof(int));
-    
-    clock_gettime(CLOCK_MONOTONIC, &start);
-    timsort(copia, TAMANHO_VETOR);
-    clock_gettime(CLOCK_MONOTONIC, &end);
 
-    double tempo = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
-    printf("Com vetor aleatorio tamanho %d, demorou %fs\n", TAMANHO_VETOR, tempo);
+    int *invertido = malloc(TAMANHO_VETOR_MAX * sizeof(int));
+    if (invertido == NULL) {
+        printf("Erro de memória!\n");
+        return 1;
+    }
+
+    int *vetor_semelhante = ler_arquivo("../../../Dados/valores-semelhantes.dat", TAMANHO_VETOR_MAX);
+    if (vetor_semelhante == NULL) {
+        printf("Erro na leitura do arquivo!\n");
+        return 1;
+    }
+
+    int *ordenado = malloc(TAMANHO_VETOR_MAX * sizeof(int));
+    if (ordenado == NULL) {
+        printf("Erro de memória!\n");
+        return 1;
+    }
     
-    free(copia);
+    //i até 5 porque o tamanho vai de 10^2 a 10^6
+    for(int i = 0; i < 5; i++){
+    //-----------------------Ordenação vetor aleatório-----------------------
+        tempo = 0;
+        total = 0;
+        printf("-------Tamanho %i-------\n", tamanho);
+        printf("..........Aleatorio.........\n");
+        for(int j = 0; j < 10; j++){
+
+            memcpy(copia, vetor, tamanho * sizeof(int));
+            
+            clock_gettime(CLOCK_MONOTONIC, &start);
+            timsort(copia, tamanho);
+            clock_gettime(CLOCK_MONOTONIC, &end);
+
+            tempo = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+            printf("...  Tempo %i: %.6fs  ...\n", j, tempo);
+            total += tempo;
+        }
+        double media = total / 10;
+        printf("...   Media: %.6fs   ...\n", media);
+        printf("............................\n");
+
+    //-----------------------Ordenação vetor ordenado------------------------
+        tempo = 0;
+        total = 0;
+        printf("..........Ordenado..........\n");
+
+        memcpy(ordenado, vetor, tamanho * sizeof(int));
+        timsort(ordenado, tamanho);
+
+        for(int j = 0; j < 10; j++){
+
+            //Aparentemente se usar o mesmo vetor já ordenado pra todas iterações, removemendo essa copia,
+            //o programa fica levemente mais rápido a cada iteração consecutiva, porque fica em cache
+            //Essa cópia "desnecessária" é só pra tirar esse aproveitamento de cache
+            memcpy(copia, ordenado, tamanho * sizeof(int));
+
+            clock_gettime(CLOCK_MONOTONIC, &start);
+            timsort(copia, tamanho);
+            clock_gettime(CLOCK_MONOTONIC, &end);
+
+            tempo = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+            printf("...  Tempo %i: %.6fs  ...\n", j, tempo);
+            total += tempo;
+        }
+        double media = total / 10;
+        printf("...   Media: %.6fs   ...\n", media);
+        printf("............................\n");
+
+    //-----------------------Ordenação vetor invertido-----------------------
+        tempo = 0;
+        total = 0;
+        for (int k = 0; k < tamanho; k++) {
+            invertido[k] = ordenado[tamanho - 1 - k];
+        }
+        printf("..........Invertido.........\n");
+        for(int j = 0; j < 10; j++){
+
+            memcpy(copia, invertido, tamanho * sizeof(int));
+            clock_gettime(CLOCK_MONOTONIC, &start);
+            timsort(copia, tamanho);
+            clock_gettime(CLOCK_MONOTONIC, &end);
+
+            tempo = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+            printf("...  Tempo %i: %.6fs  ...\n", j, tempo);
+            total += tempo;
+        }
+        double media = total / 10;
+        printf("...   Media: %.6fs   ...\n", media);
+        printf("............................\n");
+
+    //-----------------------Ordenação vetor semelhante----------------------
+        tempo = 0;
+        total = 0;
+        
+        printf(".....Valores Semelhantes....\n");
+        for(int j = 0; j < 10; j++){
+
+            memcpy(copia, vetor_semelhante, tamanho * sizeof(int));
+            clock_gettime(CLOCK_MONOTONIC, &start);
+            timsort(copia, tamanho);
+            clock_gettime(CLOCK_MONOTONIC, &end);
+
+            tempo = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+            printf("...  Tempo %i: %.6fs  ...\n", j, tempo);
+            total += tempo;
+        }
+        double media = total / 10;
+        printf("...   Media: %.6fs   ...\n", media);
+        printf("............................\n");
+
+    //-----------------------------------------------------------------------
+        tamanho = tamanho*10;
+    }
     free(vetor);
+    free(copia);
+    free(invertido);
+    free(vetor_semelhante);
+    free(ordenado);
     return 0;
 }
