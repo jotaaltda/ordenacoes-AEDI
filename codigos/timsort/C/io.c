bd0eaa6ac993af5616a188109441d352a5bf84b9
ThalesGuerreiro <thalesguerreiroc@gmail.com>
2026-05-06 00:15:42 -0300

    Altera o método de leitura dos arquivos Deixa um pouco mais rápido/eficiente

--- codigos/timsort/C/io.c
+++ codigos/timsort/C/io.c
@@ -5,7 +5,7 @@
 int* ler_arquivo(const char* caminho, int tamanho){
     FILE *fp = fopen(caminho, "r");
     if(fp == NULL){
-        printf("Erro ao abrir o arquivo!/n");
+        printf("Erro ao abrir o arquivo!\n");
         return NULL;
     }
 
@@ -17,14 +17,24 @@ int* ler_arquivo(const char* caminho, int tamanho){
         return NULL;
     }
 
+    char linha[64];
+
     for(int i = 0; i < tamanho; i++){
-        if (fscanf(fp, "%d", &vetor[i]) != 1) {
+        if (fgets(linha, sizeof(linha), fp) == NULL) {
             printf("Erro ao ler o arquivo na posição %d\n", i);
             fclose(fp);
             free(vetor);
             return NULL;
         }
+
+        if (sscanf(linha, "%d", &vetor[i]) != 1) {
+            printf("Erro ao converter linha %d\n", i);
+            fclose(fp);
+            free(vetor);
+            return NULL;
+        }
     }
+    
     fclose(fp);
     return vetor;
 }
