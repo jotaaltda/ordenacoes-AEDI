1dbe6678c85e46678b340f408eabbde762f65e34
Joao Paulo <jotape.hin@gmail.com>
2026-05-09 06:54:24 -0300

    Implementa introsort.h para CPP

---
+++ codigos/introsort/CPP/introsort.h
@@ -0,0 +1,34 @@
+#ifndef INTROSORT_H
+#define INTROSORT_H
+
+#include <string>
+
+struct Resultado {
+    double tempo;
+    size_t memoria;
+};
+
+std::vector<int> lerArquivo(
+    const std::string& nomeArquivo,
+    int quantidade
+);
+
+void introsort(
+    std::vector<int>& vetor
+);
+
+void inverterVetor(
+    std::vector<int>& vetor
+);
+
+Resultado executarOrdenacao(
+    std::vector<int>& vetor
+);
+
+void imprimirResultado(
+    const std::string& arquivo,
+    int tamanho,
+    Resultado r
+);
+
+#endif
